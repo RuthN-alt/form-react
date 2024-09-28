63b10e5b9460529b47285f2791fa92497638d26e
Ruth Naomi <imoruth746@gmail.com>
2024-09-28 04:01:59 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormComponent from './formcomponent';
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Form </h1>
+            <FormComponent />
+        </div>
+    );
+};
+
+export default App;
